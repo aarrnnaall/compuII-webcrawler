d3ab357a04e2dce3cc33d1deada01811a82c3167
aarrnnaall <arnal26@hotmail.com>
2019-07-17 16:44:02 -0300

    parseo

--- parseo.py
+++ parseo.py
@@ -1,5 +1,4 @@
 from HTMLParser import HTMLParser 
-<<<<<<< HEAD
 
 class MyHTMLParser(HTMLParser):
 
@@ -9,21 +8,4 @@ class MyHTMLParser(HTMLParser):
             if cont_sinesp[0:6] != '!funct' and cont_sinesp[0:2] != '/*' and cont_sinesp[0:4] != '.orb':
                 if cont_sinesp[0:3] != '.ve' and cont_sinesp[0:4] != 'func' and cont_sinesp[0:3] != 'var':
                     if cont_sinesp[0:2] != "//":
-                        print(cont_sinesp)
-
-        
-
-=======
-
-class MyHTMLParser(HTMLParser):
-
-    def handle_data(self, data):
-        cont_sinesp = ' '.join(data.split()) 
-        if cont_sinesp != "" and cont_sinesp[0:6] != 'jQuery' and cont_sinesp[0:6] != 'window' and cont_sinesp[0:6] != '(funct':
-            if cont_sinesp[0:6] != '!funct' and cont_sinesp[0:2] != '/*' and cont_sinesp[0:4] != '.orb':
-                if cont_sinesp[0:3] != '.ve' and cont_sinesp[0:4] != 'func' and cont_sinesp[0:3] != 'var':
-                    if cont_sinesp[0:2] != "//":
-                        print(cont_sinesp)
-
-        
->>>>>>> baa358b0632988031d7baa23e9fdb373c06cfe6a
+                        print(cont_sinesp) 
