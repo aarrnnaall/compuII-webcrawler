25a000c1e48c328b6626314d403d574c0ea99046
aarrnnaall <arnal26@hotmail.com>
2019-07-19 18:27:23 -0300

    detalle_consulta

--- consulta.py
+++ consulta.py
@@ -4,16 +4,30 @@ class consultahtml(object):
 
     def __init__(self,palabra):
         buff_buscar = [] 
+        palabra_esp= palabra.split(" ")
         files = open('cont.txt', 'r') 
         mapear = mmap.mmap(files.fileno(), 0, access=mmap.ACCESS_READ) 
-        while(True):
-            linea = mapear.readline()
-            if palabra in linea:
-                buff_buscar.append(linea)
-            if not linea:
-                break
-        buff = "".join(buff_buscar)
-        print(buff) 
+        #for palabra_bus in palabra_esp: 
+        for palabra_bus in palabra_esp:
+            while(True):
+                a=palabra_bus
+                linea = mapear.readline()
+                if a in linea:
+                    print("---------------------------------------------------------------"+a)
+                    buff_buscar.append(linea)
+                    a=None
+                else:
+                    if a.title() in linea:
+                        buff_buscar.append(linea)
+                        a=None
+                if not linea:
+                    break
+         
+        if buff_buscar == []:
+            print("No se encontro Nada")
+        else:
+            buff = "".join(buff_buscar)
+            print(buff) 
     
 
             
