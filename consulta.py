87d6680130dc295f162e5ebdf7b63161a888c5ba
Lucho <lucho_94_1994@hotmail.com>
2020-07-09 12:02:48 -0300

    eliminando_redundancia

--- consulta.py
+++ consulta.py
@@ -16,15 +16,23 @@ class MiHilocons(threading.Thread):
 
     def consult(self):
         link = glob('*.txt')
-        for linea in fileinput.input(link):
-            if self.palabra in linea:
-                print(linea)
-            if not linea:
-                break
+        if(link):
+             input = fileinput.input(link)
+             for linea in input:
+                if self.palabra in linea:
+                    print(linea)
+                if not linea:
+                    break
+             input.close()
+        else:
+             return False
 
     def run(self):
-        self.consult()
-        #self.cond.acquire()
-        #self.cond.wait()
-        #self.cond.release()
+        if(self.consult()==False):
+            print("Esperando que se carge")
+            self.cond.acquire()
+            self.cond.wait()
+            self.consult()
+            self.cond.release()
+            print("Ya cargado!")
 
